f96a636550db584990a6eb5521eca1c029e887b5
kitamuraDev <masato1999824@icloud.com>
2022-03-13 21:44:57 +0900

    :sparkles: 基本的なtodo処理を実装

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+let todos = [];
+
 /* get top page */
-router.get('/', function (_req, res, _next) {
-  res.render('index', { title: 'ExTodo' });
+router.get('/', (_req, res) => {
+  res.render('index', {
+    title: 'ExTodo',
+    todos: todos,
+  });
+});
+
+/** post input todo content */
+router.post('/', (req, res) => {
+  const todo = req.body.add;
+  todos.push(todo);
+  res.redirect('/');
 });
 
 module.exports = router;
